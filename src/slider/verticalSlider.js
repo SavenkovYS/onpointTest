d185ad1110f7aecb584a9139301b8b836b27b871
Юрий Савенков <yura-savenkov1@yandex.ru>
2021-04-12 19:32:51 +0300

    vertical slides

--- src/slider/verticalSlider.js
+++ src/slider/verticalSlider.js
@@ -1,6 +1,8 @@
 (function() {
     function slideDown() {
         const verticalSlides = document.querySelectorAll('.vertical-slide')
+        const paginationBtns = document.querySelectorAll('.page-main__pagination')
+        console.log(paginationBtns)
         const slider = document.querySelector('.slider')
         let currentSlide = 0
         let transformValue = 0;
@@ -16,15 +18,30 @@
                 if (finalCoord - 50 < initCoord && currentSlide !== verticalSlides.length - 1) {
                     currentSlide++
                     slider.style.transform = `translateY(${-100 / verticalSlides.length * currentSlide}%)`
+                    paginationBtns.forEach((btn, i) => {
+                        btn.classList.remove('page-main__pagination--active')
+                        if (i === currentSlide) {
+                            btn.classList.add('page-main__pagination--active')
+                        }
+                    })
                 }
 
                 if (finalCoord > initCoord + 50 && currentSlide !== 0) {
                     currentSlide--
                     slider.style.transform = `translateY(${-100 / verticalSlides.length * currentSlide}%)`
+                    paginationBtns.forEach((btn, i) => {
+                        btn.classList.remove('page-main__pagination--active')
+                        if (i === currentSlide) {
+                            btn.classList.add('page-main__pagination--active')
+                            return
+                        }
+                    })
                 }
                 slider.removeEventListener('touchend', handleTouchend)
             }
         })
+
+
     }
     
 
